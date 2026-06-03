fac26cc08963ccc7bad6b222ae1cb895efe0b08a
Antony Buffone <antonybuffone@gmail.com>
2026-06-03 19:00:49 +0200

    Use TOPFLY red brand palette

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  themeColor: "#301247",
+  themeColor: "#f31317",
 };
 
 export default function RootLayout({
